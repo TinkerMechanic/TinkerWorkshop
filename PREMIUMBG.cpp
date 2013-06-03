b22e8d4954383eb64644107f4434ff68212a1791
TinkerMechanic <tinker-tech@outlook.com>
2013-06-03 16:22:13 +0300

    Update PREMIUMBG.cpp

--- PREMIUMBG.cpp
+++ PREMIUMBG.cpp
@@ -4,27 +4,35 @@
 * re-released by anyone besides the original owner of the script. 
 * Any kind of breaking those rules will be reported.
 */
- 
+
 #include "Battleground.cpp"
 #include "Battleground.h"
 #include "Chat.h"
 #include "Chathandler.cpp"
 #include "ScriptPCH.h"
-
+ 
 #define SPELL_ARENA_PREPARATION "14185"
 #define NOTIFICATION_COLOR "cff9f3434"
  
 class PremiumBGPrivileges : public PlayerScript
-  {
-  public:
-    PremiumBGPrivileges() : PlayerScript("PremiumBGPrivileges"){}
-	  {
-				 void PremiumBG(Player* player /*type*/)
-					 if (inArena())
-					 if(player->GetSession()->GetSecurity() == SEC_PREMIUM) /* Checks if the player is PREMIUM. */
-						player->CastSpell(SPELL_ARENA_PREPARATION); 
-				 player->GetSession->SendNotification("NOTIFICATION_COLOR [Premium] You're spells has been resetted.");
+{
+    public:
+        PremiumBGPrivileges() : PlayerScript("PremiumBGPrivileges") { }
+        
+        void PremiumBG(Player* player /*type*/)
+        {
+            if (player->GetBattleground())
+            {
+                if(player->GetSession()->GetSecurity() == SEC_PREMIUM) /* Checks if the player is PREMIUM. */
+                {
+                    player->CastSpell(player, 14185, true); 
+                    player->GetSession()->SendNotification("NOTIFICATION_COLOR Premium members do get privileges in the Battlegrounds, to check out those privileges talk to Master Fu inside the battleground."); 
+                }
+            }
+        }
+};
+ 
 void AddSC_PremiumBGPrivileges()
 {
-        new PremiumBGPrivileges();
+    new PremiumBGPrivileges();
 }
